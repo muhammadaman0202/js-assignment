2d21b0bade9b21d95a9581e42c71f79659669554
szaj <jafrisyedzaheerabbas@gmail.com>
2021-05-28 11:19:58 +0500

    js assignment with new

---
+++ app.js
@@ -0,0 +1,349 @@
+// CHAPTER NO# 1
+
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+// alert("Hello World .");
+
+
+
+
+// 2. Write a script to display following message on your web
+// page:
+
+// alert("Error ! Please enter a valid password .");
+
+
+
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// alert("Welcome to js Land... \n Happy Coding !")
+
+
+
+
+
+
+// 4. Write a script to display following messages in sequence:
+
+
+// alert("Welcome to js Land...")
+// alert("Happy Coding !")
+
+
+
+
+
+// 5. Generate the following message through browser’s
+// developer console:\
+
+
+// alert("Hello ! i can run js through my Chrome console");
+
+
+
+
+
+
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// ALERTS | JAVASCRIPT
+// Page 3 of 3
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+
+// ..........................................................................................
+
+
+// CHAPTER NO# 2
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+
+// 1. Declare a variable called username.
+// var username;
+
+
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// var myName ;
+// myName = "Muhammad Aman" ;
+
+
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var message = "Hello World"
+// alert(message);
+
+
+
+// . Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+// var firstName = "Muhammad";
+// var lastName = " Aman";
+// var age =  20;
+// var qualification = "  Intermediate"
+
+// alert(firstName +   lastName +  age + qualification);
+
+
+
+// Write a script to display the following alert using one JS
+// variable:
+
+
+// var fastFood = "pizza \n pizz \n piz \n pi \n p" ;
+// alert(fastFood);
+
+
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+
+// var email = "muhammadamansaleem@gmail.com";
+
+// alert("My Email Address is  " + email);
+
+
+
+
+
+
+
+// . Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// var book = "A smarter \n way to learn JavaScript";
+// alert(  "i ma trying to learn from the book " + book);
+
+
+
+
+
+// 8. Write a script to display this in browser through JS
+
+// document.write("Yah ! i can Write HTML content through Javascript")
+
+
+
+
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+// alert(" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ");
+
+
+// .......................................................................
+
+
+
+// CHAPTER NO# 3
+
+
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+
+// var age = 20 ;
+// alert("I am " + age + " Years Old");
+
+
+
+
+
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// 
+// 
+
+
+
+
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// var birthYear = 2001 ;
+// document.write("My Birthyear is " + birthYear ); 
+// document.write( "Datatype of my declared variable is  " + typeof birthYear);
+
+
+
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+
+// var visitorName = "John Doe  ";
+// var productTitle = "Ordered  ";
+// var orderProducts = 5 ;
+// document.write(visitorName + productTitle + orderProducts + "T-shirt(s)  " +  "Bonanza Clothing Store");
+
+
+
+
+// .........................................................................
+
+// CHAPTER NO# 4
+
+
+// 1. Declare 3 variables in one statement.
+// 2. Declare 5 legal & 5 illegal variable names.
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+
+// var firstName = "Muhammad " , 
+// fatherName = "Aman " ,
+// age = 20 ;
+// alert(firstName + fatherName + age);
+
+
+
+
+
+// legal variable names.
+
+// Start them with a letter
+// $character
+// _character
+// characternumeric(1 to ...)
+// userName  (Camel Case)
+
+
+
+
+// illegal variable names.
+// & 
+// %
+// 1
+// *
+// space
+
+
+
+
+
+
+
+// a) A heading stating “Rules for naming JS variables”
+
+// Variable names cannot contain spaces.
+// Variable names must begin with a letter, an underscore _ or a dollar sign $ 
+// Variable names can only contain letters, numbers, underscores, or dollar signs.
+// Variable names are case-sensitive.
+
+
+// b) Variable names can only contain __$____, ___(_)___,
+// __letters____ and __numbers____.
+
+// c) Variables must begin with a ___letter___, ___$charcater___ or
+// __$character___.      For example $name, _name or name
+
+// d) Variable names are case ___Sensitive______
+
+// e) Variable names should not be JS ___(keywords)______
+
+
+
+
+
+
+// .....................................................................................................
+
+// CHAPTER NO# 5
+
+
+
+
+
+
+// 1. Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+
+
+// var sum1 = 10
+// var sum2 = 15
+// var sum3 = (sum1+sum2);
+// alert(sum3);
+
+
+
+
+
+
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+
+
+// var sum1 = 10
+// var sum2 = 15
+// var sum3 = (sum1-sum2);
+// alert(sum3);
+
+
+
+// var sum1 = 10
+// var sum2 = 15
+// var sum3 = (sum1*sum2);
+// alert(sum3);
+
+
+
+// var sum1 = 10
+// var sum2 = 15
+// var sum3 = (sum1/sum2);
+// alert(sum3);
+
+
+
+
+
